8db690ff92f955a1b5bfc9a595636631c1a1d229
Sirui Sun <siruisun@microsoft.com>
2024-08-15 11:25:22 +0800

    Updates

---
+++ Custom-Function-Using-WebWorker/commands.js
@@ -0,0 +1,2 @@
+Office.onReady((function(){})),Office.actions.associate("action",(function(e){var i={type:Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,message:"Performed action.",icon:"Icon.80x80",persistent:!0};Office.context.mailbox.item.notificationMessages.replaceAsync("action",i),e.completed()}));
+//# sourceMappingURL=commands.js.map
